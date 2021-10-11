60e8296b0e4fcab83ee76284762c89a028eddf75
Juan Diego Gonzalez <jd.gonzalezg1@uniandes.edu.co>
2021-10-10 21:18:53 -0500

    Version inicial

--- Cliente/cliente.py
+++ Cliente/cliente.py
@@ -1,24 +1,43 @@
 import socket, threading, hashlib, time, os
 from datetime import datetime
 
-# Socket UDP
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+if __name__ == "__main__":
+    try:
+        # Se establece la cantidad de clientes que se van a crear
+        cantThreads = int(input("Ingrese la cantidad de clientes a crear: "))
+        if cantThreads < 1:
+            raise ValueError("[Error] El numero debe ser mayor a 0")
 
-host = input("Ingrese la direccion IP del servidor (esta fue indicada en la terminal donde se ejecuto el servidor): ")
-port = 1234
-server_address = (host, port)
-message = b'This is the message.'
+        # Se crea la carpeta para guardar los archivos (si no existe)
+        if not os.path.isdir('ArchivosRecibidos'):
+            os.mkdir(os.path.join(os.getcwd(), "ArchivosRecibidos"))
 
-try:
-    # Send data
-    print('sending {!r}'.format(message))
-    sent = sock.sendto(message, server_address)
+        # Se crea la carpeta para guardar los logs (si no existe)
+        if not os.path.isdir('Logs'):
+            os.mkdir(os.path.join(os.getcwd(), "Logs"))
 
-    # Receive response
-    print('waiting to receive')
-    data, server = sock.recvfrom(4096)
-    print('received {!r}'.format(data))
 
-finally:
-    print('closing socket')
-    sock.close()
+
+
+        # Se crea el socket UDP del cliente
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+        host = input("Ingrese la direccion IP del servidor (esta fue indicada en la terminal donde se ejecuto el servidor): ")
+        port = 1234
+        server_address = (host, port)
+        message = b'This is the message.'
+
+        # Send data
+        print('sending {!r}'.format(message))
+        sent = sock.sendto(message, server_address)
+
+        # Receive response
+        print('waiting to receive')
+        data, server = sock.recvfrom(4096)
+        print('received {!r}'.format(data))
+
+        print('closing socket')
+        sock.close()
+
+    except (ValueError, ConnectionResetError) as e:
+        print("\n", e, sep="")
--- Servidor/servidor.py
+++ Servidor/servidor.py
@@ -3,6 +3,19 @@ from datetime import datetime
 
 if __name__ == "__main__":
     try:
+        # Se carga el contenido del archivo
+        nombreArchivo = input("\nIngrese el nombre del archivo a transferir (incluyendo la extension): ")
+        print("Cargando archivo...")
+        archivo = open("ArchivosAEnviar/{}".format(nombreArchivo), "rb")
+        contenidoArchivo = archivo.read()
+        archivo.close()
+        print("Archivo cargado")
+
+        # Se establece la cantidad de clientes a atender al tiempo
+        cantConexiones = int(input("\nIngrese la cantidad de conexiones concurrentes: "))
+        if cantConexiones < 1:
+            raise ValueError("[Error] El numero debe ser mayor a 0")
+
         # Se crea el socket UDP del servidor
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         host = None
@@ -24,6 +37,20 @@ if __name__ == "__main__":
         sock.bind((host, port))
         print("\nDireccion IP del servidor:", host)
 
+        # Se crea la carpeta para guardar el log (si no existe)
+        if not os.path.isdir('Logs'):
+            os.mkdir(os.path.join(os.getcwd(), "Logs"))
+
+
+
+
+        # Se inicializan las listas de clientes
+        resultComprobacionHash = [None for i in range(cantConexiones)]
+        tiemposDeTransmision = [None for i in range(cantConexiones)]
+
+        print("\nServidor listo para atender clientes")
+
+        # Se
         while True:
             print('\nwaiting to receive message')
             data, address = sock.recvfrom(4096)
@@ -36,5 +63,6 @@ if __name__ == "__main__":
                 sent = sock.sendto(data, address)
                 print('sent {} bytes back to {}'.format(
                     sent, address))
+
     except (FileNotFoundError, ValueError, ConnectionResetError) as e:
         print("\n", e, sep="")
