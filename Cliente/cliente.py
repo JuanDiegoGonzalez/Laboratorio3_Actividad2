5ff825a0ca46cb3b1286eb43adb0c5c52091c814
Juan Diego Gonzalez <jd.gonzalezg1@uniandes.edu.co>
2021-10-10 20:37:22 -0500

    Version inicial

--- Cliente/cliente.py
+++ Cliente/cliente.py
@@ -4,11 +4,12 @@ from datetime import datetime
 # Socket UDP
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-server_address = ('localhost', 10000)
-message = b'This is the message.  It will be repeated.'
+host = input("Ingrese la direccion IP del servidor (esta fue indicada en la terminal donde se ejecuto el servidor): ")
+port = 1234
+server_address = (host, port)
+message = b'This is the message.'
 
 try:
-
     # Send data
     print('sending {!r}'.format(message))
     sent = sock.sendto(message, server_address)
--- Servidor/servidor.py
+++ Servidor/servidor.py
@@ -1,23 +1,40 @@
 import socket, threading, hashlib, time, os, platform, subprocess, ipaddress
 from datetime import datetime
 
-# Socket UDP
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+if __name__ == "__main__":
+    try:
+        # Se crea el socket UDP del servidor
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        host = None
 
-# Bind the socket to the port
-server_address = ('localhost', 10000)
-print('starting up on {} port {}'.format(*server_address))
-sock.bind(server_address)
+        try:  # Se intenta identificar la IP automaticamente
+            if platform.system() == 'Windows':
+                p = subprocess.Popen('ipconfig', shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+                host = str(p.stdout.readlines()[7]).split(" ")[-1].split("\\")[0]  # host = 192.168.0.2 (o similar)
+            elif platform.system() == 'Linux':
+                p = subprocess.Popen('ifconfig', shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+                host = str(p.stdout.readlines()[1]).split("inet")[1].split(" ")[1]  # host = 192.168.0.16 (o similar)
 
-while True:
-    print('\nwaiting to receive message')
-    data, address = sock.recvfrom(4096)
+            ipaddress.IPv4Network(host)
 
-    print('received {} bytes from {}'.format(
-        len(data), address))
-    print(data)
+        except:  # Si no se puede (es decir, si hay error), se le pregunta al usuario
+            host = input("\nIngrese la direccion IP de esta maquina (ejecutar el comando ipconfig o ifconfig en una terminal): ")
 
-    if data:
-        sent = sock.sendto(data, address)
-        print('sent {} bytes back to {}'.format(
-            sent, address))
+        port = 1234
+        sock.bind((host, port))
+        print("\nDireccion IP del servidor:", host)
+
+        while True:
+            print('\nwaiting to receive message')
+            data, address = sock.recvfrom(4096)
+
+            print('received {} bytes from {}'.format(
+                len(data), address))
+            print(data)
+
+            if data:
+                sent = sock.sendto(data, address)
+                print('sent {} bytes back to {}'.format(
+                    sent, address))
+    except (FileNotFoundError, ValueError, ConnectionResetError) as e:
+        print("\n", e, sep="")
